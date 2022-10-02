7f07d661c3ca9c947f24624f08f10e9e11072380
gmanriqe <gmanriqe@gmail.com>
2022-10-02 17:26:51 -0500

    Proyecto inicial (sin configuracion)

---
+++ 01-preguntas.js
@@ -0,0 +1,184 @@
+// Tipos de Testing en el Frontend
+/*
+    1.Pruebas unitarias (unit testing)
+    2.pruebas de integracion
+    3.pruebas E2E
+*/
+
+// 1.Pruebas unitarias (unit testing)
+/*
+    - Se enfocan en las pruebas mas pequeñas de nuestro proyecto (componentes reuzables, componentes globales)
+    - Probar componentes por separado, quiere decir que no puedes hacer una prueba unitaria para probar 10 componentes
+*/
+
+// 2.pruebas de integracion
+/*
+    - Se base en la union de +1 componente y verificar su funcionalidad y la seguridad de los componentes que han sido integrados
+*/
+
+// 3.pruebas E2E (Son las mas costosas y llevan mas tiempo en escribirlas)
+/*
+    - Se encarga de promprobar flujos completos de la apliacion de inicio a fin. Ejm.: Transferencia a un banco (comprobamos desde el momento que el usuario ingresa el numero de tarjeta hasta que termina la transaccion)
+    Verificamos que todas las vistas funcionen correctamenta.
+*/
+
+// Que es Test Runner
+/*
+    - Nos ayudan a implementar ambientes de pruebas para poder ejecutar nuestros componentes y colaborar su funcionamiento de manera automatica  
+    - Existen muchos "test runner" y nosotros utilizaremos Jest para Javascript
+*/
+
+// Ejemplos de Test Runner para Unit Testing en Javascript
+/*
+    Jest (Creado por el equipo de facebook, y es quien tambien le crea Features al framework), mocha, Jazmin y ava
+*/
+
+// Que es Jest
+/*
+    - Es un Test Runner para ejecutar un ambiente de pruebas y podamos probar el funcionamiento de nuestros componentes
+*/
+
+// Que nos aportar Jest
+/*
+    - Zero config (Lo trae implementado create-react-app)
+    - Snapshots (es una copia del arbol de tu componente que lo guardas en la carpeta de pruebas unitarias. Compara el componente del UI y el snapshot que se genera a la hora de ejecutar la prueba). Si es diferente, entonces algo cambio en tu UI inesperadamente
+    - Isolated Test (todas las pruebas que ejecutamos en Jest, son aisladas del ambiente en desarrollo)
+    - Api (La documentacion es muy buena y podemos ir a consultarla siempre)
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+
+// ===============================
+// Comandos
+// ===============================
+// para correr nuestro test: (configuracion por defecto con create-react-app)
+/*
+    npm run test
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
+
+//
+/*
+*/
